4a5c07d93d4e1cb707e88c56b9769e4c2559bf7e
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-28 22:05:37 +0200

    init

---
+++ GridLayout/GridLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridLayoutApp.swift
+//  GridLayout
+//
+//  Created by Baptiste MARCON on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct GridLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
