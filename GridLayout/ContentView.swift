bb5486dd34982f21cdcc26f07d5be655b05faa05
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-28 22:25:59 +0200

    feat(grid): add slider and grid layout

--- GridLayout/ContentView.swift
+++ GridLayout/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let animals = ["🐈","🐆","🦌","🦒","🦏","🐄","🐀","🦩"]
+    
+    @State private var sliderValue: CGFloat = 2
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        let columns: [GridItem] = Array(repeating: .init(.flexible()), count: Int(self.sliderValue))
+        
+        return VStack {
+            
+            Slider(value: $sliderValue, in: 2...10, step: 1)
+            Text(String(format: "%.0f", self.sliderValue))
+            
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(self.animals, id: \.self) {animal in
+                        Text(animal).font(.system(size: 100))
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
